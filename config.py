969e2de683e6abba5f6f5828fc4bef3feb18757f
mutiarakhr <mutiara.kusuma.hapsari.raharjo@gmail.com>
2026-05-02 11:19:19 +0700

    fix load_sheets function

--- config.py
+++ config.py
@@ -15,8 +15,28 @@ def get_client():
 
     return gspread.authorize(creds)
 
-
 gc = get_client()
+
+def load_sheets(gc):
+    sheet_id_source = "1ekb_YgpSlzHUwkGrnOVXf8XjnPxadRTyIlabhCzPRrM"
+
+    spreadsheet = gc.open_by_key(sheet_id_source)
+
+    sheets = {}
+
+    for ws in spreadsheet.worksheets():
+        data = ws.get_all_values()
+
+        if not data:
+            continue
+
+        header = data[0]
+        rows = data[1:]
+
+        df = pd.DataFrame(rows, columns=header)
+        sheets[ws.title] = df
+
+    return sheets
 
 sheet_id_source = "1ekb_YgpSlzHUwkGrnOVXf8XjnPxadRTyIlabhCzPRrM"
 sheet_id_cleaned = "1TjH5yD8CI4m6t0Vb5nSiHXEUz0k69i07tNFpomC1ktI"
