60a3f85b62d49a5ae8cac95f8e9cab6119c87bb2
mutiarakhr <mutiara.kusuma.hapsari.raharjo@gmail.com>
2026-05-02 11:31:31 +0700

    fix add functions

--- export.py
+++ export.py
@@ -1,6 +1,37 @@
 import pandas as pd
 from config import CLEANED_MAP
 
+def clean_for_gsheets(df):
+    df = df.copy()
+
+    for col in df.columns:
+        if pd.api.types.is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].astype(str)
+
+    df = df.map(lambda x: str(x) if isinstance(x, pd.Timestamp) else x)
+    df = df.fillna("")
+
+    return df
+
+
+def write_to_sheet(spreadsheet, sheet_name, df):
+
+    df = clean_for_gsheets(df)
+
+    try:
+        worksheet = spreadsheet.worksheet(sheet_name)
+        worksheet.clear()
+    except:
+        worksheet = spreadsheet.add_worksheet(
+            title=sheet_name,
+            rows=str(len(df) + 10),
+            cols=str(len(df.columns) + 10)
+        )
+
+    data = [df.columns.tolist()] + df.values.tolist()
+    worksheet.update(data)
+
+
 def export_cleaned(cleaned_data, spreadsheet):
 
     for key, sheet_name in CLEANED_MAP.items():
